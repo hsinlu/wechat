b505f418f8ee8efadf221974e043207becf6909d
hsinlu <hsinlu@live.com>
2015-11-18 20:06:29 +0800

    封装异常处理

--- src/Hsin/Wechat/Func/CallbackIP.php
+++ src/Hsin/Wechat/Func/CallbackIP.php
@@ -22,9 +22,7 @@ trait CallbackIP
 			]
 		]);
 
-		if (property_exists($json, 'errcode') && $json->errcode != 0) {
-			throw new WechatException($json->errmsg, $json->errcode);
-		}
+		$this->exceptionOrNot($json);
 
 		return $json->ip_list;
 	}
